155e00cf7ff87bdffb6c9b91ab75a4003fb19625
tincerti <tnincerti@gmail.com>
2017-02-20 12:11:02 -0800

    Trevor Working Branch
    Updated visuals for daily mean absolute returns plot

--- code/02-desc-stats.R
+++ code/02-desc-stats.R
@@ -9,19 +9,33 @@ theme_set(theme_bw())
 pre <- post <- 200
 dr <- matrix(NA, nrow = pre + post + 1, ncol = nrow(regime.change))
 for (i in 1:nrow(regime.change)){
-  dr[, i] <- return_by_td(stockdata = index,
-                        event_ticker = regime.change$ticker[i],
-                        event_date = regime.change$stock_date[i], pre_event = pre, 
-                        post_event = post)  
+  dr[, i] <- return_by_td(stockdata = index[ticker == regime.change$ticker[i], .(date, dr)],
+                          event_date = regime.change$stock_date[i], pre_event = pre, 
+                          post_event = post)  
 }
 
-# exclude 2 events that don't have complete data 
+# note: 2 events don't have complete data and are excluded
 dr.na <- apply(dr, 2, function (x) sum(1 * is.na(x)))
-abs.dr.mean <- apply(abs(dr[, which(dr.na == 0)]), 1, mean)
-
-# plot
+abs.dr.mean <- apply(abs(dr[, which(dr.na==0)]), 1, mean)
 tmp <- data.table(dr = abs.dr.mean, td = seq(-pre, post))
+
+# Create plot
 p <- ggplot(tmp, aes(x = td, y = dr)) + 
-  geom_segment(mapping = aes(xend = td, yend = 0)) +
-  xlab("Trading days") + ylab("Absolute value of daily return (%)")
+  geom_area(color="gray48", fill='gray48', size = .1) +
+  xlab("Trading days") + ylab("Absolute value of daily return (%)") +  
+  
+  theme(
+    plot.background = element_blank()
+    ,panel.grid.major = element_blank()
+    ,panel.grid.minor = element_blank()
+    ,panel.border = element_blank()) +
+  
+  theme(axis.line.x = element_line(color="black"),
+        axis.line.y = element_line(color="black"),
+        axis.ticks = element_blank()) +
+  
+  theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")) +
+  
+  scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0))
+
 ggsave("figs/daily_mean_absreturn.pdf", p, height = 5, width = 7)
